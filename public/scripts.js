95e21d18b285b2f3813cee30f6eb27b31244707e
cuongphama1 <74636566+cuongphama1@users.noreply.github.com>
2024-06-16 18:02:35 +0700

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const apiUrl = 'http://localhost:5000/api/students';
+    const apiUrl = '172.31.11.214:30002';
 
     const studentTable = document.getElementById('studentTable').getElementsByTagName('tbody')[0];
     const studentForm = document.getElementById('studentForm');
